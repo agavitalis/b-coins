20cb742b66c5522ac44ba95cd64223bcbd638504
Ogbonna Vitalis <o.vitalis@genesystechhub.com>
2019-04-26 23:31:34 +0100

    Uploading files to git

--- controllers/connection.php
+++ controllers/connection.php
@@ -5,7 +5,17 @@ $password = "";
 $dbname = "ogbuke";
 
 // Create connection
-$conn = new mysqli($servername, $user, $password,$dbname);
+//mysql://b88876b959129e:ecd442ef@us-cdbr-iron-east-02.cleardb.net/heroku_d63663c785fdadb?reconnect=true
+
+//connection for heroku
+$servername_live = "us-cdbr-iron-east-02.cleardb.net";
+$user_live = "b88876b959129e";
+$password_live = "ecd442ef";
+$dbname_live = "heroku_d63663c785fdadb";
+// Create connection
+
+//$conn = mysqli_connect($servername, $user, $password,$dbname);
+$conn = mysqli_connect($servername_live, $user_live, $password_live,$dbname_live);
 
 // Check connection
 if ($conn->connect_error) {
